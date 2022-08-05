8e3ae089bc3effaa12258495da875bb885200bba
alia9087 <pariialia203@gmail.com>
2022-08-05 20:42:19 +0500

    first commit

---
+++ build/precache-manifest.f9400aac095341a66584df8fee4e3041.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "892f77b0e2cc9331e2c8",
+    "url": "/static/css/main.709ba0d6.chunk.css"
+  },
+  {
+    "revision": "892f77b0e2cc9331e2c8",
+    "url": "/static/js/main.892f77b0.chunk.js"
+  },
+  {
+    "revision": "429b4b30962d8b3c4874",
+    "url": "/static/css/1.09403aaf.chunk.css"
+  },
+  {
+    "revision": "429b4b30962d8b3c4874",
+    "url": "/static/js/1.429b4b30.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36894d5e1fe0163f725cd64333e61e96",
+    "url": "/static/media/p1.36894d5e.png"
+  },
+  {
+    "revision": "6ea661156df41436b874d0b0e79b3ca0",
+    "url": "/static/media/side_logo.6ea66115.png"
+  },
+  {
+    "revision": "a8eaf2ea9e82c0891987073c5d118ee5",
+    "url": "/static/media/31.a8eaf2ea.png"
+  },
+  {
+    "revision": "b012b456c4bea5f4dcc5dc677f336f89",
+    "url": "/index.html"
+  }
+];
